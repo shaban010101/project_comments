183fd99405a042ff192bd5ae755c524614162f5e
Shaban <shaban.karumba@mymedsandme.com>
2024-09-13 15:59:32 +0200

    Add the project status

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,22 @@ class ProjectsController < ApplicationController
     @comments = @project.comments.order(created_at: :desc)
     @histories = @project.project_histories.order(created_at: :desc)
   end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated"
+      redirect_to @project
+    else
+      flash[:error] = @project.errors.full_messages
+      render :show
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:status)
+  end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  let(:project) { create(:project) }
+
   describe "show" do
     context "when the project exists" do
-      let(:project) { create(:project) }
-
-      it "gives back an ok status" do
+      it "gives back an ok status", :aggregate_failures do
         get :show, params: { id: project.id }
         expect(response).to have_http_status(:ok)
         expect(assigns(:project)).to be_a(Project)
@@ -20,5 +20,27 @@ RSpec.describe ProjectsController, type: :controller do
         expect(response).to have_http_status(:not_found)
       end
     end
+  end
+
+  describe "#update" do
+    context "when the project is updated" do
+      it "redirects back to the project page", :aggregate_failures do
+        update_project(project, "complete")
+        expect(response).to redirect_to(project_path(project))
+        expect(project.reload.status).to eq("complete")
+      end
+    end
+
+    context "when the project has not been updated" do
+      it "redirects back to the project page", :aggregate_failures do
+        update_project(project, "completed")
+        expect(response).to render_template("projects/show")
+        expect(project.reload.status).to eq("ready")
+      end
+    end
+  end
+
+  def update_project(project, status)
+    patch :update, params: { id: project.id, project: { status: status} }
   end
 end
